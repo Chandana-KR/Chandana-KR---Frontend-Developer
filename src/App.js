c34b34957354b1bddecc25c129a377bb83dad950
Chandu <chandanakr437@gmail.com>
2024-10-15 20:44:01 +0000

    Features-about-section-updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import Home from './components/Home'
+import FeaturesSection from './components/FeaturesSection'
+import AboutSection from './components/AboutSection'
 
 import './App.css'
 
-const App = () => <Home />
+const App = () => (
+  <>
+    <Home />
+    <FeaturesSection />
+    <AboutSection />
+  </>
+)
 
 export default App
